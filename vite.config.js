3bb0a6077414c67c8be1fe42a5835b17e6dfc57f
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2023-09-20 17:11:54 +0200

    modifs pour deploiement

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,13 +6,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    //vue(),
+    [vue()],
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // },
   //ajout de ces lignes pour le deploiement
   base: "/ExerciceCopieSiteResto/",
   plugins: [],
