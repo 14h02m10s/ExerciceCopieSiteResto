143e344d850c0a6e1301ff958691ac9a14d7ecdc
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2023-09-21 10:31:17 +0200

    test modif l'alias dans vite config

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,8 @@ export default defineConfig({
   resolve: {
     alias: {
       // '@': fileURLToPath(new URL('./src', import.meta.url))
-      '@': fileURLToPath(new URL('./ExerciceCopieSiteResto/src', import.meta.url))
+      '@': fileURLToPath(new URL('.', import.meta.url))
+      // '@': fileURLToPath(new URL('./ExerciceCopieSiteResto/src', import.meta.url))
     }
   },
 })
