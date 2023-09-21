93581910d6cccd4f957871091e481ea3421140ed
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2023-09-21 10:38:37 +0200

    test supp alias et plugins ds vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,28 @@
 // import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   //ajout de cette ligne pour le deploiement
   base: "/ExerciceCopieSiteResto/",
-
-  plugins: [
-    vue(),
-    // [vue()],
-  ],
-  // plugins: [],
-  // resolve: {
-  //   alias: [
-
-  //   ]
-  // }
+  plugins: [],
   resolve: {
-    alias: {
-      // '@': fileURLToPath(new URL('./src', import.meta.url))
-      // '@': fileURLToPath(new URL('.', import.meta.url))
-      // '@': fileURLToPath(new URL('./ExerciceCopieSiteResto/src', import.meta.url))
-    }
-  },
+    alias: [
+
+    ]
+  }
+
+  // plugins: [
+  //   vue(),
+  //   // [vue()],
+  // ],
+  // resolve: {
+  //   alias: {
+  //     // '@': fileURLToPath(new URL('./src', import.meta.url))
+  //     // '@': fileURLToPath(new URL('.', import.meta.url))
+  //     // '@': fileURLToPath(new URL('./ExerciceCopieSiteResto/src', import.meta.url))
+  //   }
+  // },
 })
